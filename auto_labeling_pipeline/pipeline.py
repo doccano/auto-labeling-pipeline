91a210b15c85c96259a0e218beb35c3fdaf4edad
Hironsan <light.tree.1.13@gmail.com>
2020-11-09 08:37:00 +0900

    Add a pipeline function

---
+++ auto_labeling_pipeline/pipeline.py
@@ -0,0 +1,14 @@
+from auto_labeling_pipeline.models import RequestModel
+from auto_labeling_pipeline.mappings import MappingTemplate
+from auto_labeling_pipeline.postprocessing import PostProcessor
+
+
+def pipeline(text: str,
+             request_model: RequestModel,
+             mapping_template: MappingTemplate,
+             post_processing: PostProcessor):
+    request = request_model.build()
+    response = request.send(text)
+    labels = mapping_template.render(response)
+    labels = post_processing.transform(labels)
+    return labels
