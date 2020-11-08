40cd4fe0171cbfef83330a212476df0cf6700500
Hironsan <light.tree.1.13@gmail.com>
2020-11-09 08:24:01 +0900

    Add labels to wrap mapped response

---
+++ auto_labeling_pipeline/label.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class ClassificationLabel(BaseModel):
+    label: str
+
+
+class SequenceLabel(BaseModel):
+    label: str
+    start_offset: int
+    end_offset: int
+
+
+class Seq2seqLabel(BaseModel):
+    label: str
