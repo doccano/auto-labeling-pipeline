d9a920044708af5be0eb8ab7c093a67c82bcf253
Hironsan <light.tree.1.13@gmail.com>
2020-11-11 14:45:10 +0900

    Annotate send as abstractmethod

--- auto_labeling_pipeline/request.py
+++ auto_labeling_pipeline/request.py
@@ -16,6 +16,7 @@ class Request(metaclass=abc.ABCMeta):
     def __init__(self, **kwargs):
         pass
 
+    @abc.abstractmethod
     def send(self, text: str):
         raise NotImplementedError
 
