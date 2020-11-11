718bca57fdc1c7d3c34fe7c542d6a6b47c52ec0a
Hironsan <light.tree.1.13@gmail.com>
2020-11-11 13:47:01 +0900

    Specify return type to pipeline

--- auto_labeling_pipeline/pipeline.py
+++ auto_labeling_pipeline/pipeline.py
@@ -1,3 +1,4 @@
+from auto_labeling_pipeline.labels import Labels
 from auto_labeling_pipeline.mappings import MappingTemplate
 from auto_labeling_pipeline.models import RequestModel
 from auto_labeling_pipeline.postprocessing import BasePostProcessor
@@ -6,7 +7,7 @@ from auto_labeling_pipeline.postprocessing import BasePostProcessor
 def pipeline(text: str,
              request_model: RequestModel,
              mapping_template: MappingTemplate,
-             post_processing: BasePostProcessor):
+             post_processing: BasePostProcessor) -> Labels:
     request = request_model.build()
     response = request.send(text)
     labels = mapping_template.render(response)
