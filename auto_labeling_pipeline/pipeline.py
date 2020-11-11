b6743c0f283fb0e417f9d36ec81a98fa5ba2c312
Hironsan <light.tree.1.13@gmail.com>
2020-11-11 13:44:24 +0900

    Define base post processor

--- auto_labeling_pipeline/pipeline.py
+++ auto_labeling_pipeline/pipeline.py
@@ -1,12 +1,12 @@
 from auto_labeling_pipeline.mappings import MappingTemplate
 from auto_labeling_pipeline.models import RequestModel
-from auto_labeling_pipeline.postprocessing import PostProcessor
+from auto_labeling_pipeline.postprocessing import BasePostProcessor
 
 
 def pipeline(text: str,
              request_model: RequestModel,
              mapping_template: MappingTemplate,
-             post_processing: PostProcessor):
+             post_processing: BasePostProcessor):
     request = request_model.build()
     response = request.send(text)
     labels = mapping_template.render(response)
--- auto_labeling_pipeline/postprocessing.py
+++ auto_labeling_pipeline/postprocessing.py
@@ -4,11 +4,18 @@ from typing import Optional, Set
 from auto_labeling_pipeline.labels import Labels
 
 
-class PostProcessor(metaclass=abc.ABCMeta):
+class BasePostProcessor(metaclass=abc.ABCMeta):
 
     def __init__(self, stop_labels: Optional[Set] = None, mapping: Optional[dict] = None):
         self.stop_labels = stop_labels
         self.mapping = mapping
+
+    @abc.abstractmethod
+    def transform(self, labels: Labels) -> Labels:
+        raise NotImplementedError
+
+
+class PostProcessor(BasePostProcessor):
 
     def transform(self, labels: Labels) -> Labels:
         labels = labels.filter_by_name(self.stop_labels)
