856110c6a0ec3a90bbc4da3e6649755e63f78691
Hironsan <light.tree.1.13@gmail.com>
2020-11-13 08:47:35 +0900

    Add test case for amazon comprehend sentiment request

--- tests/test_request.py
+++ tests/test_request.py
@@ -2,7 +2,7 @@ import os
 
 import vcr
 
-from auto_labeling_pipeline.models import GCPEntitiesRequestModel
+from auto_labeling_pipeline.models import AmazonComprehendSentimentRequestModel, GCPEntitiesRequestModel
 
 
 def test_gcp_entities_request(cassettes_path):
@@ -11,3 +11,18 @@ def test_gcp_entities_request(cassettes_path):
         request = model.build()
         response = request.send(text='Google, headquartered in Mountain View')
         assert 'entities' in response
+
+
+def test_amazon_comprehend_sentiment_request(cassettes_path):
+    with vcr.use_cassette(str(cassettes_path / 'amazon_comprehend_sentiment.yaml'),
+                          mode='once',
+                          filter_headers=['authorization']):
+        model = AmazonComprehendSentimentRequestModel(
+            aws_access_key=os.environ.get('AWS_ACCESS_KEY', ''),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY', ''),
+            region_name='us-east-1',
+            language_code='en'
+        )
+        request = model.build()
+        response = request.send(text='I am very sad.')
+        assert 'Sentiment' in response
