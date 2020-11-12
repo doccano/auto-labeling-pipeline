d6ba0695049a159be1ca7e94b0afcd52c7b399e7
Hironsan <light.tree.1.13@gmail.com>
2020-11-12 14:40:41 +0900

    Add menu to search options

---
+++ auto_labeling_pipeline/menu.py
@@ -0,0 +1,51 @@
+import abc
+from typing import List, Literal, Type
+
+from pydantic import BaseModel
+
+from auto_labeling_pipeline.mappings import AmazonComprehendSentimentTemplate, GCPEntitiesTemplate, MappingTemplate
+from auto_labeling_pipeline.models import (AmazonComprehendSentimentRequestModel, CustomRESTRequestModel,
+                                           GCPEntitiesRequestModel, RequestModel)
+from auto_labeling_pipeline.postprocessing import BasePostProcessor, PostProcessor
+
+
+class Option(BaseModel, abc.ABC):
+    name: str
+    task: Literal['Any', 'TextClassification', 'SequenceLabeling', 'Seq2seq']
+    model: Type[RequestModel]
+    template: Type[MappingTemplate]
+    post_processor: Type[BasePostProcessor] = PostProcessor
+
+
+class Options:
+    options = [
+        Option(
+            name='Custom Rest Request',
+            task='Any',
+            model=CustomRESTRequestModel,
+            template=MappingTemplate
+        ),
+        Option(
+            name='Amazon Comprehend Sentiment Analysis',
+            task='TextClassification',
+            model=AmazonComprehendSentimentRequestModel,
+            template=AmazonComprehendSentimentTemplate
+        ),
+        Option(
+            name='GCP Entity Analysis',
+            task='SequenceLabeling',
+            model=GCPEntitiesRequestModel,
+            template=GCPEntitiesTemplate
+        )
+    ]
+
+    @classmethod
+    def filter_by_task(cls, task: str) -> List[Option]:
+        return list(filter(lambda o: o.task in {task, 'Any'}, cls.options))
+
+    @classmethod
+    def find(cls, option_name: str) -> Option:
+        for option in cls.options:
+            if option.name == option_name:
+                return option
+        raise ValueError('Option {} is not found.'.format(option_name))
---
+++ tests/test_menu.py
@@ -0,0 +1,34 @@
+import pytest
+
+from auto_labeling_pipeline.menu import Options
+
+
+@pytest.mark.parametrize(
+    'task, expected',
+    [
+        ('TextClassification', {'Any', 'TextClassification'}),
+        ('XXX', {'Any'})
+    ]
+)
+def test_filter_task(task, expected):
+    options = Options.filter_by_task(task=task)
+    tasks = {o.task for o in options}
+    assert tasks == expected
+
+
+@pytest.mark.parametrize(
+    'option_name',
+    [
+        'Custom Rest Request',
+        'Amazon Comprehend Sentiment Analysis',
+        'GCP Entity Analysis'
+    ]
+)
+def test_find_option(option_name):
+    option = Options.find(option_name)
+    assert option.name == option_name
+
+
+def test_find_invalid_option():
+    with pytest.raises(ValueError):
+        Options.find('XXX')
