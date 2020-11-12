3b916f957c7581f3341e45772580a582f50dab7a
Hironsan <light.tree.1.13@gmail.com>
2020-11-13 08:16:15 +0900

    Add test case for GCP request

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,8 @@ import pytest
 @pytest.fixture
 def data_path():
     return pathlib.Path(__file__).parent / 'data'
+
+
+@pytest.fixture
+def cassettes_path():
+    return pathlib.Path(__file__).parent / 'fixtures/cassettes'
---
+++ tests/test_request.py
@@ -0,0 +1,13 @@
+import os
+
+import vcr
+
+from auto_labeling_pipeline.models import GCPEntitiesRequestModel
+
+
+def test_gcp_entities_request(cassettes_path):
+    with vcr.use_cassette(str(cassettes_path / 'gcp_entities.yaml'), mode='once', filter_query_parameters=['key']):
+        model = GCPEntitiesRequestModel(key=os.environ.get('API_KEY_GCP'), type='PLAIN_TEXT', language='en')
+        request = model.build()
+        response = request.send(text='Google, headquartered in Mountain View')
+        assert 'entities' in response
