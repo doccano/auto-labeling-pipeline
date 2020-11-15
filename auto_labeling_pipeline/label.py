6dc5c6a2350d99a85ca44ff9ab6ad02543bfc108
Hironsan <light.tree.1.13@gmail.com>
2020-11-15 13:10:31 +0900

    Add an abstract class to label

--- auto_labeling_pipeline/label.py
+++ auto_labeling_pipeline/label.py
@@ -1,15 +1,49 @@
+import abc
+from typing import Set
+
 from pydantic import BaseModel
 
 
-class ClassificationLabel(BaseModel):
+class Label(BaseModel, abc.ABC):
+
+    @abc.abstractmethod
+    def included(self, labels: Set[str]) -> bool:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def replace(self, mapping: dict):
+        raise NotImplementedError
+
+
+class ClassificationLabel(Label):
     label: str
 
+    def included(self, labels: Set[str]) -> bool:
+        return self.label in labels
 
-class SequenceLabel(BaseModel):
+    def replace(self, mapping: dict):
+        if self.label in mapping:
+            self.label = mapping[self.label]
+
+
+class SequenceLabel(Label):
     label: str
     start_offset: int
     end_offset: int
 
+    def included(self, labels: Set[str]) -> bool:
+        return self.label in labels
 
-class Seq2seqLabel(BaseModel):
+    def replace(self, mapping: dict):
+        if self.label in mapping:
+            self.label = mapping[self.label]
+
+
+class Seq2seqLabel(Label):
     text: str
+
+    def included(self, labels: Set[str]) -> bool:
+        return self.text in labels
+
+    def replace(self, mapping: dict):
+        pass
