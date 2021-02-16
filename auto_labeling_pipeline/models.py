286c7b3d6a8244671c027ed17f0dc1bc01dc0701
Hironsan <light.tree.1.13@gmail.com>
2021-02-16 14:19:06 +0900

    Update AmazonComprehendSentimentRequestModel

--- auto_labeling_pipeline/models.py
+++ auto_labeling_pipeline/models.py
@@ -64,7 +64,9 @@ class GCPEntitiesRequestModel(RequestModel):
 class AmazonComprehendSentimentRequestModel(RequestModel):
     aws_access_key: str
     aws_secret_access_key: str
-    region_name: str
+    region_name: Literal['us-east-2', 'us-east-1', 'us-west-2', 'ap-south-1', 'ap-northeast-2', 'ap-southeast-1',
+                         'ap-southeast-2', 'ap-northeast-1', 'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2',
+                         'us-gov-west-1']
     language_code: Literal['en', 'es', 'fr', 'de', 'it', 'pt', 'ar', 'hi', 'ja', 'ko', 'zh', 'zh-TW']
 
     def build(self) -> Request:
